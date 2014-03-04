7cb595e93dd885b11cbc0a3deb8681e69d246793
Hack Reactor <hackreactor@HR-A4.local>
2014-03-03 16:42:27 -0800

    changed BlinkyDancer, step no work

--- src/dancer.js
+++ src/dancer.js
@@ -14,6 +14,8 @@ var Dancer = function(top, left, timeBetweenSteps){
 
   this.step();
 
+  //this._step = this.step;
+
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
